a54a62eecfc7d13dcd60da14be6192d8742598cd
Haiz14 <haiz.gigs@gmail.com>
2023-01-14 14:06:24 +0530

    modified:   denzel/cogs/reaction_role.py 	  added admin amd fish_cake role

--- denzel/cogs/reaction_role.py
+++ denzel/cogs/reaction_role.py
@@ -11,20 +11,65 @@ class ReactionRole(commands.Cog):
 
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
+
+        # if reaction is not on role message return
         if payload.message_id != 1062994674085797908: 
             
             return
+
+        # get guold and member
+
         if payload.emoji.name == "🍥":
-            await self.bot.bot_channel.send(' role message fishcake')
-        else:
-            await self.bot.bot_channel.send('not role message id')
+            await self.add_role( role_name = "fish_cake", payload=payload)
+
+
+        elif payload.emoji.name == "🅰️": 
+
+            await self.add_role( role_name = "Admin", payload=payload)
+
+
+    @commands.Cog.listener()
+    async def on_raw_reaction_remove(self, payload):
+
+        # if reaction is not on role message return
+        if payload.message_id != 1062994674085797908: 
+            
             return
-        return
+
+        # get guold and member
+
+        if payload.emoji.name == "🍥":
+            await self.remove_role( role_name = "fish_cake", payload=payload)
+
+        elif payload.emoji.name == "🅰️": 
+
+            await self.remove_role( role_name = "Admin", payload=payload)
+
+    async def add_role(self, role_name, payload):
 
         guild = self.bot.get_guild(payload.guild_id)
         member = guild.get_member(payload.user_id)
-        role = discord.utils.get(guild.roles, name="fish_cake")
-        await member.add_roles(role)
+
+        role = discord.utils.get(guild.roles, name=role_name)
+        try:
+            await member.add_roles(role)
+            await self.bot.bot_channel.send(f"{member.mention} got {role_name} role")
+        except Exception as e:
+            await bot.bot_channel.send(f"error couldn't add role cause ```{e}```")
+            
+    async def remove_role(self, role_name, payload):
+
+        guild = self.bot.get_guild(payload.guild_id)
+        member = guild.get_member(payload.user_id)
+
+        role = discord.utils.get(guild.roles, name=role_name)
+        try:
+            await member.remove_roles(role)
+            await self.bot.bot_channel.send(f"{member.mention} left {role_name} role")
+        except Exception as e:
+            await bot.bot_channel.send(f"error couldn't remove role cause ```{e}```")
+            
+
 
 
 async def setup(bot):
